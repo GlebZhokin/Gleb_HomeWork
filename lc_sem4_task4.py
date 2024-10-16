4a367fccf39d42432232c0600c3c9572efa44aa2
GlebZhokin <jockin2015@gmail.com>
2024-10-16 18:09:00 +0300

    Update lc_sem4_task4.py

--- lc_sem4_task4.py
+++ lc_sem4_task4.py
@@ -1,6 +1,6 @@
 spaces_string = input('Введите строку: ')
 
 while '  ' in spaces_string:
-    spaces_string.replace('  ', ' ')
+    spaces_string = spaces_string.replace('  ', ' ')
 
 print(spaces_string.strip())
